21c0c9f884dd73997c320f7c48dd190db3410191
mexeniz <mexeniz@gmail.com>
2015-11-06 19:18:43 +0700

    update python core

--- python VANET/client.py
+++ python VANET/client.py
@@ -3,9 +3,9 @@ import socket
 import time
 import datetime
 
-print "VANETs Broadcast [Simple Flooding]" 
-UDP_IP = input("Broadcast IP:")
 UDP_PORT = 5005
+print "VANETs Broadcast [Simple Flooding] on port ",UDP_PORT 
+UDP_IP = input("Broadcast IP X.X.X.255 :")
 MESSAGE = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
 print "UDP target IP:", UDP_IP
@@ -15,10 +15,19 @@ print "message:", MESSAGE
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 #sock.bind((UDP_IP, 0))
+# set up broadcast
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
+carID = "4"
+name = "RPi"
+seqNumber = 1
+positionX = 10.14
+positionY = -2.51
+
 while(1):
-	MESSAGE = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+	MESSAGE = "{0} {1} {2} {3} {4}".format(carID,name,seqNumber,positionX,positionY)
 	print "Send Beacon ",len(MESSAGE), " bytes Message: ", MESSAGE
 	sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+	seqNumber += 1
 	time.sleep(1)
+	
